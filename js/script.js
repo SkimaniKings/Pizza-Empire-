59fa13ee176683774a4b9d15dd88367effcdcfc7
skimanikings <kimanisimon856@gmail.com>
2019-11-11 00:37:31 +0300

    edit alert message upon submiiting message

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 function formValidate() {
+    event.preventDefault()
   var name = document.getElementById("name");
   var email = document.getElementById("email");
   var message = document.getElementById("message");
   if (name == " " || email == "" || message == "") {
     alert("Please enter Valid information");
   } else {
-    alert(" Hey " + name + "thank you for shopping with us.");
+    alert(" Hey, thank you for shopping with us.");
   }
 }
