10ea7f89f0b461c986f407526f1bd5df3cc6556b
skimanikings <kimanisimon856@gmail.com>
2019-11-11 16:02:10 +0300

    add an animation for the ordering on homepage

--- js/script.js
+++ js/script.js
@@ -21,20 +21,24 @@ function Results (type,size,crust,toppings) {
     this.toppings=toppings;
 };
 Results.prototype.order = function() {
-    return "You have made an order of " + this.type + " pizza  with " + this.toppings + " as toppings " + this.crust + " for crust ."
+    return "You have made an order of " + this.type + " pizza  with " + this.toppings + " as toppings and " + this.crust + " for crust ."
 };
-function TotalPrice (price, quantity, delivery,toppings) {
+function TotalPrice (price, quantity, delivery,toppings,crust) {
     this.price= price;
     this.quantity=quantity;
     this.delivery=delivery;
     this.toppings=toppings;
+    this.crust=crust;
 };
 TotalPrice.prototype.finalTotal = function () {
-    return this.price * this.quantity + this.delivery + this.toppings;
+    return this.price * this.quantity + this.delivery + this.toppings + this.crust;
 };
 
 var sizePrices = [1200, 800, 600]
 var priceToppings = [100,120,80,150,200]
+var toppingsName= ["Pepperoni" , "mushroom" , "onion" ,"sausage", " bacon"]
+var crustNames= ["Crispy", "Stuffed", "Glutton-free"]
+var crustPrices = [100,120,200]
 var deliveryPrices = [0, 200];
 $(document).ready(function(){
     $('form#myForm').submit(function (event) {
@@ -42,24 +46,29 @@ $(document).ready(function(){
     var pizzaType = $('#type').val();
     var pizzaSize = parseInt($('#size').val());
     var pizzaToppings=parseInt($('#top').val());
-    var pizzaCrust = $('#crusting').val();
- var pizzaTop = $('#top').val();
+    var priceCrust =parseInt($('#crusting').val());
+    var pizzaTop = $('#top').val();
     var pizzaQty = parseInt($('#quantity').val());
     var pizzaPick = parseInt($('#delivery').val());
     var price = sizePrices[pizzaSize - 1];
     var DeliveryCost = deliveryPrices[pizzaPick - 1];
-    var ToppingsCost = priceToppings[pizzaToppings-1]
-    newOrder = new Results(pizzaType,pizzaSize, pizzaCrust, pizzaTop);
-    newTotal = new TotalPrice(price, pizzaQty, DeliveryCost,ToppingsCost);
+    var ToppingsCost = priceToppings[pizzaToppings-1];
+    var crustCost = crustPrices[priceCrust-1]
+    var topNames = toppingsName[pizzaTop-1]
+    var crustName = crustNames[priceCrust-1] 
+    newOrder = new Results(pizzaType,pizzaSize, crustName,topNames,crustName);
+    newTotal = new TotalPrice(price, pizzaQty, DeliveryCost,ToppingsCost,crustCost);
     if (pizzaPick===1){
-        alert("You have ordered: " + newOrder.order());
+        alert( newOrder.order());
         alert("Your bill is: " + newTotal.finalTotal());
+        alert("Thank you for shopping with us! " )
         }else{
             if(pizzaPick===2){
                 prompt(" Where would your like you Pizza to be Delivered? ");
                 alert("Your order has been received and it will be delivered. An additional 200/= will be charged for delivery.");
-                alert("Your oder is: " + newOrder.order());
-                alert("Your bill is: " + newTotal.finalTotal());
+                alert(newOrder.order());
+                alert( newTotal.finalTotal());
+                alert("Thank you for shopping with us! Your Order will be arriving soon." )
             }
         }
 })
