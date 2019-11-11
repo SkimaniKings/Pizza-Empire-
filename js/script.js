8cb2350d211b219017c4764aa20d571e7adb34e5
skimanikings <kimanisimon856@gmail.com>
2019-11-11 11:54:32 +0300

    add jquery statements to js

--- js/script.js
+++ js/script.js
@@ -9,21 +9,34 @@ function formValidate() {
     alert(" Hey, thank you for shopping with us.");
   }
 };
-function results (type,size,crust,toppings) {
+function Results (type,size,crust,toppings) {
     this.type = type;
     this.size= size;
     this.crust= crust;
     this.toppings=toppings;
 };
-results.prototype.order = function() {
+Results.prototype.order = function() {
     return "You hav made an order of " + this.type + " with " + this.size + " with " + this.crust + " crust " + " and" + this.toppings + " toppings."
 };
-function totalPrice (price, quantity, delivery) {
+function TotalPrice (price, quantity, delivery) {
     this.price= price;
     this.quantity=quantity;
     this.delivery=delivery;
 };
-totalPrice.prototype.finalTotal = function () {
+TotalPrice.prototype.finalTotal = function () {
     return this.price * this.quantity + this.delivery;
 };
+
+var sizePrices = [1200, 800, 600]
+var deliveryPrices = [0, 200];
+$(document).ready(function(){
+    var pizzaType = $('#type').val();
+    var pizzaSize = parseInt($('#size').val());
+    var pizzaCrust = $('#crust').val();
+    var pizzaTop = $('#topping').val();
+    var pizzaQty = parseInt($('#quantity').val());
+    var pizzaPick = parseInt($('#delivery').val());
+    var price = sizePrices[pizzaSize - 1];
+    var DeliveryCost = deliveryPrices[pizzaPick - 1];
+});
  
