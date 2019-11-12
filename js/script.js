74aa8be29c3af072d77d8d10e261835fed9f3cf9
skimanikings <kimanisimon856@gmail.com>
2019-11-12 11:26:14 +0300

    make changes on validation of form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 function validateForm() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-       if (name == "") {
+       if (name === "") {
       alert("please fill in your name");
       return false;
-    } else if (email == "") {
+    } else if (email === "") {
       alert("Please fill in your name and email");
       return false;
     } else {
       alert(
-        name + ", we have received your message. Thank you for reaching out to us."
+        name + ", We have received your message. Thank you for reaching out to us."
       );
     }
     event.preventDefault();
